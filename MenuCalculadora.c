5677d5305b92e35dd201887342516f42aa9c39fa
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-06-15 13:28:12 -0300

    MenuCalculadora.c
    atualização do código , organizando as linahs....

--- MenuCalculadora.c
+++ MenuCalculadora.c
@@ -1,4 +1,4 @@
-//Realizar um programa em C que imprima um menu e o usuario selecione que operaÁ„o aritmÈtica deseja (adiÁ„o a, subtraÁ„o s, multiplicaÁ„o m, divis„o d)
+//Realizar um programa em C que imprima um menu e o usuario selecione que opera√ß√£o aritm√©tica deseja (adi√ß√£o a, subtra√ß√£o s, multiplica√ß√£o m, divis√£o d)
 
 
 #include <stdio.h>
@@ -8,40 +8,44 @@ int main(void)
 {
 int nResultado,nValor1,nValor2;
 char op;
-printf(" \n\n a=adicao s=subtracao \n m=multiplicacao d=divisao");
-printf("\n\n selecione uma opcao: \t");
-scanf("%s", &op);
-printf("por favor insira o valor de A= ");
-scanf("%d", &nValor1);
-printf("insira o valor de B= ");
-scanf("%d", &nValor2);
+    printf(" \n\n a=adicao s=subtracao \n m=multiplicacao d=divisao");
+       printf("\n\n selecione uma opcao: \t");
+       scanf("%s", &op);
+           printf("por favor insira o valor de A= ");
+           scanf("%d", &nValor1);
+               printf("insira o valor de B= ");
+                scanf("%d", &nValor2);
 
-switch (toupper(op))
-{
-case 'A': nResultado=nValor1+nValor2;
-if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2){
-nResultado=(nValor1+nValor2);
-printf("\n A adicao e :\t %d\n", nResultado);
-break;}
-case 'S': nResultado=nValor1-nValor2;
-if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2) {
-nResultado=(nValor1-nValor2);
-printf("\n A subtracao e :\t %d\n", nResultado);
-break;}
-case 'M': nResultado=nValor1*nValor2;
-if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2){
-nResultado=(nValor1*nValor2);
-printf("\n A multiplicacao e:\t %d\n", nResultado);
-break;}
-case 'D': nResultado=nValor1/nValor2;
-if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2)
-nResultado=(nValor1/nValor2);
-else
-nResultado=(nValor2/nValor1);
-printf("\nA divisao e :\t %d\n", nResultado);
-break;
-default: printf("\nInvalido !!!!\n");
-}
+  switch (toupper(op))
+ {
+   case 'A': nResultado=nValor1+nValor2;
+   if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2){
+   nResultado=(nValor1+nValor2);
+   printf("\n A adicao e :\t %d\n", nResultado);
+   break;}
+   
+     case 'S': nResultado=nValor1-nValor2;
+     if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2) {
+     nResultado=(nValor1-nValor2);
+     printf("\n A subtracao e :\t %d\n", nResultado);
+     break;}
+     
+       case 'M': nResultado=nValor1*nValor2;
+       if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2){
+       nResultado=(nValor1*nValor2);
+       printf("\n A multiplicacao e:\t %d\n", nResultado);
+       break;}
+       
+         case 'D': nResultado=nValor1/nValor2;
+         if(nValor1>nValor2 || nValor2>nValor1 || nValor1==nValor2)
+         nResultado=(nValor1/nValor2);
+         else
+         nResultado=(nValor2/nValor1);
+         printf("\nA divisao e :\t %d\n", nResultado);
+         break;
+
+           default: printf("\nInvalido !!!!\n");
+ }
 system ("pause");
 return 0;
 }
