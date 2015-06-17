06f08f699324c22be073763827f9b1d56532af4f
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-06-17 14:53:13 -0300

    Update alarme.c

--- alarme.c
+++ alarme.c
@@ -1,21 +1,30 @@
 /* Programa que calcula o tempo min para dois alarmes dispararem simultaneamente */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (void)
 {
-	int x,y,t;
-	printf ("Intervalo do alarme x ?:\t\n");
-	scanf ("%d", &x);
-	printf ("Intervalo do alarme y ?: \t\n");
-	scanf ("%d", &y);	
+	int x,y,t; //Declarando variáveis
 	
+	//
+	printf ("Intervalo do alarme x ?:\t");
+	scanf ("%d", &x);
+	
+	//
+	printf ("\nIntervalo do alarme y ?: \t");
+	scanf ("%d", &y);	
+    
+	//    	
 	t = 1;
 	
-	while ( t%x!=0 || t%y!=0) t++;
-	printf("Tempo min = \t %d \n",t);
+	//
+	while ( t%x!=0 || t%y!=0) 
+	t++;
+	printf("\nTempo min = \t %d \n\n\n\n",t);
 	
-system ("pause");
-return 0;
+	//pausando o programa
+	system ("pause");
+	return 0;
 
 }
