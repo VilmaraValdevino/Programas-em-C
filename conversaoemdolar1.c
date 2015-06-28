388fb7728045045d6b8390820f313ba7a9ced3ee
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-06-28 20:19:53 -0300

    Update conversaoemdolar1.c

--- conversaoemdolar1.c
+++ conversaoemdolar1.c
@@ -1,12 +1,33 @@
 /* Programa que converte valor em real para dólares */
+
 #include <stdlib.h>
+#include <locale.h>
 #include <stdio.h>
+
 int main(void) {
-float real,dolar,VRED;
-printf ("informe um valor em real e a cotação do dólar:");
-scanf ("%f %f" , &real, &dolar);
-VRED = real/dolar;
-printf ("O valor real em dólares é = %.2f\n" , VRED);
-system ("pause");
-	return 0;
+    
+    //Definindo nacionalidade 
+    setlocale(LC_ALL,"Portuguese");
+    
+    float real,dolar,valorRealEmDolar; //Definindo variáveis do tipo real. 
+    
+    // 
+    printf ("informe um valor em real: R$  ");
+    scanf ("%f" , &real);
+    
+    // 
+    printf ("informe a cotação do dólar: R$   ");
+    scanf ("%f" , &dolar);
+   
+    // 
+    valorRealEmDolar = real/dolar;
+    
+    // 
+    printf ("O valor real em dólares é = %.2f\n\n" , valorRealEmDolar);
+    
+    //Pausando o programa com a mensagem "Pressione qualquer tecla para continuar..." 
+    system ("pause");
+    
+    // 
+    return 0;
 }
