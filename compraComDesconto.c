9c00e6edb3266cfaea31369cb62e5ef435f1ed81
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-07-01 18:22:55 -0300

    Update and rename desconto.c to compraComDesconto.c

---
+++ compraComDesconto.c
@@ -0,0 +1,33 @@
+/* Programa que calcula desconto concebido e informa valor final à ser pago na compra */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (void)
+{
+    //Definindo a nacionalidade
+    setlocale(LC_ALL,"Portuguese");
+
+    // 
+    float valorOriginal, percentualDeDesconto, valorAserPago; //Declarando variáveis do tipo real
+    
+    // 
+    printf (" informe o valor original da compra: R$ ");
+    scanf ("%f" , &valorOriginal);
+
+    printf (" informe o percentual de desconto: ");
+    scanf ("%f" , &percentualDeDesconto);
+    
+    // 
+    valorAserPago = valorOriginal - ((percentualDeDesconto*valorOriginal)/100);
+    
+    // 
+    printf (" O valor à ser pago é: R$ %.2f\n\n\n", valorAserPago);
+    
+    //Pausando o programa e exibindo a ensagem "Pressione qualquer tecla para continua..." 
+    system ("pause");
+    
+    // 
+    return 0;
+}
