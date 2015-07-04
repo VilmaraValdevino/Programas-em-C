20f395a0d80d8727224c6c0a50ead92fb246d451
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-07-04 20:09:39 -0300

    Update idosos.c

--- idosos.c
+++ idosos.c
@@ -1,26 +1,39 @@
-/* Programa que calcula a idade dos pacientes e verifica dentre eles qual � o mais idoso */
+/* Programa que calcula a idade dos pacientes e verifica dentre eles qual é o mais idoso */
+
 #include<stdio.h>
 #include<stdlib.h>
+#include<locale.h>
 
 int main(void)
 {
-int nIdosos,idadeIdoso,maisIdoso = 0,contador = 1;  
-printf("Digite a quantidade de idosos:\t");
-scanf("%d",&nIdosos);
-  for(contador; contador <= nIdosos; contador++)
-    {
-     printf("Digite a idade do idoso:\t");
-     scanf("%d",&idadeIdoso);
-
-     if(idadeIdoso > maisIdoso)
-         {
-         maisIdoso = idadeIdoso;
-	     }
-    }
-
-    printf("O mais idoso tem %d anos\n", maisIdoso);
+    //Definindo a nacionalidade 
+    setlocale(LC_ALL,"Potuguese");
     
-system("pause");
-return 0;
+    int nIdosos,idadeIdoso,maisIdoso = 0,contador = 1; //Declarando as variáveis do tipo inteiro
+    
+    // 
+    printf("Digite a quantidade de idosos:\t");
+    scanf("%d",&nIdosos);
+    
+    // 
+    for(contador; contador <= nIdosos; contador++)
+    {
+        printf("Digite a idade do idoso:\t");
+        scanf("%d",&idadeIdoso);
+         
+        if(idadeIdoso > maisIdoso)
+        {
+            maisIdoso = idadeIdoso;
+	    }
+    }
+    
+    // 
+    printf("\nO mais idoso tem %d anos\n\n\n", maisIdoso);
+    
+    //Pausando o programa e exibindo a mensagem "Pressione qualquer tecla para continuar..." 
+    system("pause");
+    
+    // 
+    return 0;
 
 }
