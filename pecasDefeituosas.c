d1bf26d2f3c0f4ef1dde81219d77d82c9b29c95d
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2015-07-17 16:35:45 -0300

    Update and rename pecasdefeituosas.c to pecasDefeituosas.c

---
+++ pecasDefeituosas.c
@@ -0,0 +1,44 @@
+/* Programa que verifica apartir da quantidade de peças produzidas quantas dessas peças estão defeituosas e se a quantidade de peças defeituosas for superior à 10%, informa que a máquina necessita de reparo para melhor desempenho! */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (void) 
+{
+    // Definindo a nacionalidade
+    setlocale(LC_ALL,"Portuguese");
+    
+    int totalPecasDefeituosas,pecasProduzidas,pecasDefeituosas; // Declarando variáveis do tipo inteiro 
+    
+    // 
+    printf ("Peças Produzidas?\t");
+    scanf ("%d", &pecasProduzidas); 
+    
+    // 
+    printf ("Peças Defeituosas?\t");
+    scanf ("%d", &pecasDefeituosas);
+    
+    // 
+    totalPecasDefeituosas = pecasDefeituosas*100/pecasProduzidas;
+    
+    // 
+    printf ("Total de Peças defeituosas em porcentagem: %.2d \n\n",totalPecasDefeituosas);
+    
+    // 
+    if (totalPecasDefeituosas>10)
+    {
+      printf (" Máquina necessita de manutenção ! \n\n\n\n\n");
+    }
+       
+    else 
+    {
+      printf (" Máquina não precisa de manutenção ! \n\n\n\n\n ");
+    }
+    
+    // Pausando o sistema e exibe a mensagem "Pressione qualquer tecla para continuar..."
+    system ("pause");
+    
+    // 
+    return 0;
+}
